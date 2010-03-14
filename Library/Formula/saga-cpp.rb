f4d5bb6d874752a1a404b4f566630290a36105d1
luckow <luckow@computer.local>
2010-03-14 10:05:06 +0100

    New formula: saga-cpp
    SAGA is an API that provides the basic functionality required to build distributed applications, tools and frameworks so as to be independent of the details of the underlying infrastructure.

---
+++ Library/Formula/saga-cpp.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class SagaCpp <Formula
+  url 'http://downloads.sourceforge.net/project/saga/SAGA%20C%2B%2B%20Implementation/1.4.1/saga-cpp-1.4.1.src.tar.gz'
+  #head 'https://svn.cct.lsu.edu/repos/saga/trunk/'
+  homepage 'http://saga.cct.lsu.edu'
+  md5 'b05c76761f79d64df1af834986e095a8'
+
+  depends_on 'boost'
+  depends_on 'postgresql'
+  depends_on 'sqlite'
+  depends_on 'xmlrpc-c'
+  
+  aka :saga
+  
+  def install
+    system "./configure", "--prefix=#{prefix}", "--with-boost=#{HOMEBREW_PREFIX}", "--with-postgresql=#{HOMEBREW_PREFIX}", "--with-sqlite3=#{HOMEBREW_PREFIX}"
+    system "make install"
+  end
+end
