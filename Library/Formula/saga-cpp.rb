bdbebc50f513c023d7a8c7c89e253c16d73fc5f0
luckow <luckow@computer.local>
2010-03-14 10:05:06 +0100

    New formula: saga-cpp
    SAGA is an API that provides the basic functionality required to build distributed applications, tools and frameworks so as to be independent of the details of the underlying infrastructure.
    Support HEAD option
    
    fixed head option

--- Library/Formula/saga-cpp.rb
+++ Library/Formula/saga-cpp.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class SagaCpp <Formula
   url 'http://downloads.sourceforge.net/project/saga/SAGA%20C%2B%2B%20Implementation/1.4.1/saga-cpp-1.4.1.src.tar.gz'
-  #head 'https://svn.cct.lsu.edu/repos/saga/trunk/'
+  head 'https://svn.cct.lsu.edu/repos/saga/trunk/'
   homepage 'http://saga.cct.lsu.edu'
   md5 'b05c76761f79d64df1af834986e095a8'
 
@@ -17,4 +17,13 @@ class SagaCpp <Formula
     system "./configure", "--prefix=#{prefix}", "--with-boost=#{HOMEBREW_PREFIX}", "--with-postgresql=#{HOMEBREW_PREFIX}", "--with-sqlite3=#{HOMEBREW_PREFIX}"
     system "make install"
   end
+  
+  def download_strategy
+    if ARGV.flag? '--HEAD'
+      SubversionDownloadStrategy
+    else
+      CurlDownloadStrategy
+    end
+  end
+  
 end
